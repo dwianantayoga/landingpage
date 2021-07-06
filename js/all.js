af2063d4474e0ff5fbf69a6556c542eb5642e759
dwianantayoga <dwi.ananta@undiksha.ac.id>
2021-07-06 12:17:54 +0800

    perbaikan nilai

---
+++ js/all.js
@@ -0,0 +1,9 @@
+//buat buka sidebar dilayar hp
+function sidebarOpen(){
+    document.getElementById("sidebar").style.transform = "translate(0px";
+}
+
+//buat buka sidebar dilayar hp
+function slidebarCLose(){
+    document.getElementById("sidebar").style.transform = "translate(300px)";
+}
